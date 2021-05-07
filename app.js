78acbce7e76cf8ed6deebb89736f50e0257e002c
rjmo <radiorafael@hotmail.com>
2021-05-07 19:18:20 -0400

    redis create

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const redis =  require('redis');
+
+
+const client = redis.createClient();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/search', function(req, res, next) {
+router.post('/search', function(req, res, next) {
   let id = req.body.id;
-  console.log(id);
+
+  client.hgetall(id, function(err, obj){
+    if(!obj){
+      res.render('index', {
+        error: 'User does not exist.'
+      });
+    } else {
+      obj.id = id;
+      res.render('details', {
+        user:obj
+      });
+    }
+  });
+});
+router.get('/add', function(req, res, next){
+  res.render('addUser')
+});
+router.post('/add', function(req, res, next){
+  
+  const { id, email, first_name, last_name, phone } = req.body;
+
+  client.hmset(id, [
+    'first_name', first_name,
+    'last_name', last_name,
+    'email', email,
+    'phone', phone
+  ], function(err, reply) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(reply)
+      res.redirect('/');
+    }
+  })
 });
 
 module.exports = router;
